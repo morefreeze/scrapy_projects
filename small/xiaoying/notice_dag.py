d238e48925f64fc1044a1ef2fbde2cb6acc28497
morefreeze <morefreeze@gmail.com>
2017-01-17 16:57:08 +0800

    Change data exchange with xcom && add some rule

--- small/xiaoying/notice_dag.py
+++ small/xiaoying/notice_dag.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from StringIO import StringIO
 from os import path, environ
 from airflow import DAG
 from airflow.operators import BashOperator, BranchPythonOperator, PythonOperator, SlackAPIPostOperator
@@ -13,8 +14,7 @@ def need_run(ti, execution_date, **kwargs):
     if ti.start_date and execution_date and ti.start_date - execution_date < long_time:
         return 'run_spider'
     return ''
-
-slack_txt_file = 'txt_for_slack'
+slack_message_key = 'slack_message'
 csv_file = 'xy.csv'
 dir = path.abspath(path.dirname(__file__))
 def filter_data(csv_file, start_day=28, end_day=90, interest=780, state=None, **kwargs):
@@ -22,7 +22,7 @@ def filter_data(csv_file, start_day=28, end_day=90, interest=780, state=None, **
     f['sub_title'] = f['sub_title'].fillna('')
     candidate = []
     filter = Filter()
-    filter.install_rule(lambda v: v['period'] <= datetime.timedelta(days=20), ok_stop=True, weight=5)
+    filter.install_rule(lambda v: v['period'] <= datetime.timedelta(days=20) and v['benefit'] > 6, ok_stop=True, weight=5)
     filter.install_rule(lambda v: v['benefit'] >= 8 and v['period'] < datetime.timedelta(days=230))
     filter.install_rule(lambda v: not v['sub_title'].startswith('新手专享'))
     for row in f.iterrows():
@@ -45,20 +45,17 @@ def filter_data(csv_file, start_day=28, end_day=90, interest=780, state=None, **
 
 def need_slack(ti, **kwargs):
     candidate = ti.xcom_pull(key=None, task_ids='filter_data')
-    try:
-        os.remove(slack_txt_file)
-    except:
-        pass
     if candidate and len(candidate) > 0:
-        with open(slack_txt_file, 'w') as f:
-            for can in candidate:
-                f.write('%s(%s) money: %s period: %s days benefit: %s%%\n' % (
-                    can['title'],
-                    can['sub_title'],
-                    can['money'],
-                    can['period'].days,
-                    can['benefit']
-                ))
+        s = StringIO()
+        for can in candidate:
+            s.write('%s(%s) money: %s period: %s days benefit: %s%%\n' % (
+                can['title'],
+                can['sub_title'],
+                can['money'],
+                can['period'].days,
+                can['benefit'],
+            ))
+        ti.xcom_push(slack_message_key, s.getvalue())
         return 'post_slack'
     return ''
 
@@ -78,7 +75,7 @@ default_args = {
     # 'end_date': datetime.datetime(2017, 1, 1),
 }
 
-dag = DAG('xiaoying', default_args=default_args, schedule_interval='*/5 8-22 * * *')
+dag = DAG('xiaoying', default_args=default_args, schedule_interval='* 8-22 * * *')
 
 
 only_run_now = BranchPythonOperator(
@@ -105,13 +102,8 @@ need_slack = BranchPythonOperator(
     dag=dag
 )
 
-slack_token = environ.get('SLACK_ME_TOKEN')
-try:
-    with open(slack_txt_file, 'r') as f:
-        txt = ''.join(f.readlines())
-        txt = txt.decode('utf-8')
-except:
-    txt = 'Nothing to read.'
+slack_token = environ.get('SLACK_TOKEN')
+txt = '''{{ task_instance.xcom_pull(task_ids='need_slack', key='%s').decode('utf-8') }}''' % (slack_message_key)
 post_slack = SlackAPIPostOperator(
     task_id='post_slack',
     token=slack_token,
