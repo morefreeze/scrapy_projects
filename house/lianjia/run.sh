b65f860dabaae698529cccb6366cee30e3ad9f06
morefreeze <morefreeze@gmail.com>
2017-05-15 14:56:32 +0800

    Fix run.sh

--- house/lianjia/run.sh
+++ house/lianjia/run.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 code=${1:-110000}
-for ((i=0;i < 1000; i++)); do
+for ((i=0;i < 1000; i+=1)); do
     echo "$i"
     rm -f "bizcircle_${code}.json"
-    python get_all_house.py "$@"
+    python get_all_house.py "$code"
     sleep 1
 done
