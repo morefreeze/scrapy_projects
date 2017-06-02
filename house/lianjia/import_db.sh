c07ed54ea2f08d77f2ad39c5d60d92d4c2534b02
morefreeze <morefreeze@gmail.com>
2017-06-02 13:22:55 +0800

    lianjia: Fix import_db

--- house/lianjia/import_db.sh
+++ house/lianjia/import_db.sh
@@ -6,5 +6,5 @@ for i in ${dir}/[0-9]*.json; do
     while read -r line; do
         echo "$line" | jq --compact-output --raw-output '.data.list' | mongoimport -d lianjia -c "$dir" --jsonArray --mode=upsert --upsertFields=house_code
     done < "$i"
-    jq --compact-output --raw-output '.data' "$dir/bizcircle_*.json" | mongoimport -d lianjia -c "${dir}_community" --jsonArray --mode=upsert --upsertFields=community_id
 done
+jq --compact-output --raw-output '.data' "$dir"/bizcircle_*.json | mongoimport -d lianjia -c "${dir}_community" --jsonArray --mode=upsert --upsertFields=community_id
